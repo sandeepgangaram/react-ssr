9096673ea115786b873af2d026fb69b7fe96d7f8
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-15 19:31:00 +0530

    change render to hydrate

--- src/client/client.js
+++ src/client/client.js
@@ -4,4 +4,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/Home";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.hydrate(<Home />, document.getElementById("root"));
