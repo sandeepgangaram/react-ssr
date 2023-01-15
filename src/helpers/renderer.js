54ca91d4f0bc879416bc3eeb105e58576a038f83
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-15 22:03:38 +0530

    refactor rendering functionality

---
+++ src/helpers/renderer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Home from "../client/components/Home";
+
+export default () => {
+  const content = renderToString(<Home />);
+
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head></head>
+  <body>
+    <div id='root'>${content}</div>
+    <script src="bundle.js"></script>
+  </body>
+  </html>
+  `;
+};
