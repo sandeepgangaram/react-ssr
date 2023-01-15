001b8719d7dc4f9f79000b5cd52f52b87962f667
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-15 21:51:58 +0530

    reduce server bundle size using webpack node externals

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
 config = {
   // Inform webpack that we're building a bundle
@@ -17,6 +18,7 @@ config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
